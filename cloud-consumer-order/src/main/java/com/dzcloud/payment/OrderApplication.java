826d1129075567760358e020a6d8161fbb7ed28f
Zdz_Coder <1392777056@qq.com>
2020-03-12 13:13:27 +0800

    将其他服务注册到eureka服务注册中心

--- cloud-consumer-order/src/main/java/com/dzcloud/payment/OrderApplication.java
+++ cloud-consumer-order/src/main/java/com/dzcloud/payment/OrderApplication.java
@@ -2,12 +2,14 @@ package com.dzcloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author DeZhe
  * @date 2020/3/12 - 10:28
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication {
 
     public static void main(String[] args) {
--- cloud-provider-payment/src/main/java/com/dzcloud/payment/PaymentApplication.java
+++ cloud-provider-payment/src/main/java/com/dzcloud/payment/PaymentApplication.java
@@ -2,12 +2,14 @@ package com.dzcloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author DeZhe
  * @date 2020/3/12 - 9:00
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApplication {
 
     public static void main(String[] args) {
