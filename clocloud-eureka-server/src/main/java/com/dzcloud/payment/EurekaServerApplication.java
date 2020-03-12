ecc9d713388c18f5de829aae59a7575736d0114a
Zdz_Coder <1392777056@qq.com>
2020-03-12 11:34:18 +0800

    加入eureka服务注册组件

---
+++ clocloud-eureka-server/src/main/java/com/dzcloud/payment/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.dzcloud.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author DeZhe
+ * @date 2020/3/12 - 11:23
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+
+}
